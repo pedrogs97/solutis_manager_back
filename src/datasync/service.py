076b2dd06cc2eb1712ffca62e1ef3d5241407f9d
Pedro Gustavo Santana <pedrogustavosantan97@gmail.com>
2024-03-20 15:17:17 -0300

    chore: adjusts lending template and query sync

--- src/datasync/service.py
+++ src/datasync/service.py
@@ -396,7 +396,6 @@ def update_employee_totvs(totvs_employees: List[EmployeeTotvsSchema]):
     """Updates employees from totvs"""
     db_session = get_db_session()
     updates: List[EmployeeModel] = []
-    news: List[EmployeeModel] = []
     try:
         for totvs_employee in totvs_employees:
             employee_db = (
@@ -494,10 +493,9 @@ def update_employee_totvs(totvs_employees: List[EmployeeTotvsSchema]):
                 updates.append(employee_db)
             else:
                 update_employee = EmployeeModel(**dict_employee)
-                news.append(update_employee)
+                updates.append(update_employee)
 
         db_session.add_all(updates)
-        db_session.add_all(news)
         db_session.commit()
         logger.info("Update Employee from TOTVS. Total=%s", str(len(updates)))
     except Exception as err:
